44e5e7d24ac5498f4b0e1e5c5f2d1035b11a4f76
MammadliHuseyn <54790582+MammadliHuseyn@users.noreply.github.com>
2020-12-04 09:54:22 +0400

    Add files via upload

---
+++ StringDataType/StringDataType/Extension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDataType
+{
+    public static class Extension
+    {
+        public static void CharAt(this string str,int index)
+        {
+            Console.WriteLine(str[index]);
+        }
+    }
+}
---
+++ StringDataType/StringDataType/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string str = "Huseyn";
+            //int a = str.Length;
+            //Console.WriteLine(a);
+            //Console.WriteLine(str[3]);
+            //str.CharAt(3);
+
+            //Console.WriteLine(str.Substring(1,4));
+            //Console.WriteLine(str.Contains("ey"));
+            //if (String.IsNullOrEmpty(str))
+            //    Console.WriteLine("String is empty!");
+            //else
+            //    Console.WriteLine(str);
+
+            //Console.WriteLine(str.StartsWith("Hus"));
+
+            //str = Console.ReadLine();
+            //Console.WriteLine(str.Trim());
+
+            //Console.WriteLine(str.ToUpper());
+
+            //Console.WriteLine(str.IndexOf('*'));
+
+            //Console.WriteLine(str.Replace(str,"Mammadli"));
+
+            //string s = new string(new char[] { 'H', 'u', 's', 'e', 'y', 'n' });
+            //string t = new string(new char[] { 'H', 'u', 's', 'e', 'y', 'n' });
+            //bool isEqual = t == s;
+
+            //Console.WriteLine(isEqual);
+
+            //Console.WriteLine(s.Equals(t));
+
+            //string str = "683";
+            //int a = 5;
+            //// String >> Integer  
+            //Console.WriteLine(int.Parse(str));
+            //// Integer >> String
+            //Console.WriteLine(a.ToString());
+
+            //Console.WriteLine(Convert.ToInt32(str));
+        }
+    }
+}
